7af505655c140aa6e77d54230cbe6688e2457848
tial0011 <taiwoalex34@gmail.comgit config --global user.name tial0011git config --global user.email taiwoalex34@gmail.com>
2026-09-16 09:22:23 +0100

    idid it

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+/**
+ * BWAI Firebase web-app configuration.
+ * This config is safe to expose in the browser; secure access with Firebase Rules.
+ */
+export const firebaseConfig = {
+  apiKey: "AIzaSyAgcz_IFkScwD95wHmROPfigtXUIRvXb-Q",
+  authDomain: "quizappweb-6fb8c.firebaseapp.com",
+  databaseURL: "https://quizappweb-6fb8c-default-rtdb.firebaseio.com",
+  projectId: "quizappweb-6fb8c",
+  storageBucket: "quizappweb-6fb8c.firebasestorage.app",
+  messagingSenderId: "420307677623",
+  appId: "1:420307677623:web:364a3a4491462cf7a8f832",
+  measurementId: "G-ECD70PVFSN"
+};
